097750f92c7675613777a10e70adbd210a7dccac
PhilippMatthes <github@philippmatth.es>
2019-07-05 09:31:51 +0200

    Commit current workstate and replace watchkit extension

---
+++ manni-watch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  manni-watch WatchKit Extension
+//
+//  Created by Philipp Matthes on 05.07.19.
+//  Copyright © 2019 Philipp Matthes. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
