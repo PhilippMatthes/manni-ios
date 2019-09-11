5d2f1d675d373e51b556672028a22b4569755a31
PhilippMatthes <github@philippmatth.es>
2019-09-11 11:42:19 +0200

    Reset project entirely to prepare for version 2

--- manni/Extensions/EquatableExtension.swift
+++
@@ -1,9 +0,0 @@
-// This code belongs to https://github.com/JohnSundell/SwiftTips
-
-import Foundation
-
-extension Equatable {
-    func isAny(of candidates: Self...) -> Bool {
-        return candidates.contains(self)
-    }
-}
